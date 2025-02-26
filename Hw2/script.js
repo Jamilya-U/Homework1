c149bd0b48c2b846fd1b08267e61f89c73dceb1a
Jamilya-U <urtdz61@gmail.com>
2025-02-26 05:08:56 +0300

    in modified form

---
+++ Hw2/script.js
@@ -0,0 +1,35 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+age = 2007;
+alert(age);
+name = "Брендан";
+alert(name);
+a = 10;
+b = 2;
+alert(a + b);
+result = 2 ** 5;
+alert(result);
+a = 9;
+b = 2;
+alert(a % b);
+num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+age = prompt('Сколько вам лет?');
+alert(age);
+name = "Брэнда";
+age = 20;
+isAdmin = Boolean(5);
+user = name + age + isAdmin;
+alert(user);
+name = prompt('Ваше имя');
+alert('Привет! ' + name);
+
+
+
